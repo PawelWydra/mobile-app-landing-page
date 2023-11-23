f2f9f45af43ea1195bdaf05d97b4050ec15428b3
pawel <wydrapawel@icloud.com>
2023-11-23 08:17:40 +0100

    add css to logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,16 @@
+import "./header.css"
+
 function Header() {
   return (
-    <div>
+    <div className="title">
       <img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="logo" />
-      <nav>
+      <nav className="navbar">
         <ul>
           <li>About</li>
           <li>Features</li>
           <li>Blog</li>
           <li>Pricing</li>
+          <button>Download Now</button>
         </ul>
       </nav>
     </div>
