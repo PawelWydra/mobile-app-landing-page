ff4a1cb6497923935ea964ac9df522e1975c3605
pawel <wydrapawel@icloud.com>
2023-11-23 07:30:24 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <div className="container background-image">
+      <p>hi how are you</p>
+    </div>
+  );
 }
 
 export default App;
