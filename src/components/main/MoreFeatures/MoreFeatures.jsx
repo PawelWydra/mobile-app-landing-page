5f7d80fc698465779d068592abd24224164efa6a
pawel <wydrapawel@icloud.com>
2023-11-28 17:54:06 +0100

    add business features

--- src/components/main/MoreFeatures/MoreFeatures.jsx
+++ src/components/main/MoreFeatures/MoreFeatures.jsx
@@ -1,4 +1,9 @@
 import "./MoreFeatures.css";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import { FaRegLightbulb } from "react-icons/fa6";
+import { FaBolt } from "react-icons/fa";
+import { FaBalanceScale } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa6";
 
 function MoreFeatures() {
   return (
@@ -7,23 +12,36 @@ function MoreFeatures() {
       <h1>Explore Our Business Features</h1>
       <div className="grid-container">
         <div className="powerful">
-          <i></i>
+          <FaBolt  className="icon icon-strong" />
           <h2>Powerful</h2>
           <p>Send large files without any limitations</p>
         </div>
+        <div className="img-container">
+          <div className="img-holder">
+            <img
+              src={process.env.PUBLIC_URL + "/images/business-img.png"}
+              alt="business-img"
+              className="business-img"
+            />
+            <img
+              src={process.env.PUBLIC_URL + "/images/business-qcode-img.jpg"}
+              alt="business-qcode"
+              className="business-qcode"
+            />
+          </div>
+        </div>
         <div className="smart">
-          <i></i>
+          <FaRegLightbulb className="icon icon-blob" />
           <h2>Smart</h2>
           <p>Get real-time notifications when someone shares a file with you</p>
         </div>
-        <div className="img-container"></div>
         <div className="easy-scale">
-          <i></i>
+          <FaBalanceScale  className="icon icon-scale" />
           <h2>Easy Scale</h2>
           <p>Access app reviews and ratings from your trusted network</p>
         </div>
         <div className="professional">
-          <i></i>
+          <FaUserTie  className="icon icon-professional" />
           <h2>Professional</h2>
           <p>Save time searching for apps with personalized recommendations</p>
         </div>
