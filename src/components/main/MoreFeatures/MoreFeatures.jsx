a13ce294ca0ffea1d85a65bc7a3c67314890c850
pawel <wydrapawel@icloud.com>
2023-11-29 19:03:11 +0100

    add business features

--- src/components/main/MoreFeatures/MoreFeatures.jsx
+++ src/components/main/MoreFeatures/MoreFeatures.jsx
@@ -1,9 +1,6 @@
 import "./MoreFeatures.css";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import { FaRegLightbulb } from "react-icons/fa6";
-import { FaBolt } from "react-icons/fa";
-import { FaBalanceScale } from "react-icons/fa";
-import { FaUserTie } from "react-icons/fa6";
+import { FaRegLightbulb, FaUserTie } from "react-icons/fa6";
+import { FaBolt, FaBalanceScale } from "react-icons/fa";
 
 function MoreFeatures() {
   return (
@@ -12,7 +9,7 @@ function MoreFeatures() {
       <h1>Explore Our Business Features</h1>
       <div className="grid-container">
         <div className="powerful">
-          <FaBolt  className="icon icon-strong" />
+          <FaBolt className="icon icon-strong" />
           <h2>Powerful</h2>
           <p>Send large files without any limitations</p>
         </div>
@@ -28,6 +25,11 @@ function MoreFeatures() {
               alt="business-qcode"
               className="business-qcode"
             />
+            <img
+              src="https://randomuser.me/api/portraits/women/44.jpg"
+              alt="person"
+              className="business-user"
+            />
           </div>
         </div>
         <div className="smart">
@@ -36,12 +38,12 @@ function MoreFeatures() {
           <p>Get real-time notifications when someone shares a file with you</p>
         </div>
         <div className="easy-scale">
-          <FaBalanceScale  className="icon icon-scale" />
+          <FaBalanceScale className="icon icon-scale" />
           <h2>Easy Scale</h2>
           <p>Access app reviews and ratings from your trusted network</p>
         </div>
         <div className="professional">
-          <FaUserTie  className="icon icon-professional" />
+          <FaUserTie className="icon icon-professional" />
           <h2>Professional</h2>
           <p>Save time searching for apps with personalized recommendations</p>
         </div>
