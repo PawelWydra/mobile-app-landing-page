2ed2997bbd7f14d408ac591764afa597829e0ef4
pawel <wydrapawel@icloud.com>
2023-11-30 16:59:40 +0100

    add folder.=, files and html structure

---
+++ src/components/main/steps/steps.jsx
@@ -0,0 +1,34 @@
+function Steps() {
+  return (
+    <div className="steps-container">
+      <span>3 STEPS</span>
+      <h1>You'll Love To Use It</h1>
+      <div className="flex-steps">
+        <div className="steps-item">
+          <div className="step-number"></div>
+          <h2></h2>
+          <p></p>
+          <a href="#"></a>
+        </div>
+      </div>
+      <div className="flex-steps">
+        <div className="steps-item">
+          <div className="step-number"></div>
+          <h2></h2>
+          <p></p>
+          <a href="#"></a>
+        </div>
+      </div>
+      <div className="flex-steps">
+        <div className="steps-item">
+          <div className="step-number"></div>
+          <h2></h2>
+          <p></p>
+          <a href="#"></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Steps;
