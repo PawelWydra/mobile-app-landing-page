474747feff9f2af3a466c82731183246af663879
pawel <wydrapawel@icloud.com>
2023-12-01 08:21:21 +0100

    add folders and html structure

---
+++ src/components/main/faq/faq.jsx
@@ -0,0 +1,35 @@
+function FAQ() {
+  return (
+    <div className="faq-container">
+      <div className="faq-flex">
+        <span></span>
+        <h1></h1>
+        <div className="faq-items-wrapper">
+          <div className="faq-item">
+            <i>icon</i>
+            <div className="answer-question-wrapper">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+          <div className="faq-item">
+            <i>icon</i>
+            <div className="answer-question-wrapper">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+          <div className="faq-item">
+            <i>icon</i>
+            <div className="answer-question-wrapper">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
