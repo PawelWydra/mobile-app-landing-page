81df62ee56789c9ddf3e035f3b51bb7c08f2cf73
pawel <wydrapawel@icloud.com>
2024-01-30 10:35:57 +0100

    feat: add mobileMenu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,24 @@
+import React, { useState } from "react";
 import "./header.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import MobileMenu from "../mobileMenu/mobileMenu.jsx";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
+
   return (
     <header className="title" id="header">
+      <MobileMenu isMenuOpen={isMenuOpen} closeMenu={handleClick}/>
       <img src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo" />
-      <nav>
+      <div className="md:hidden">
+        <RxHamburgerMenu size={40} onClick={handleClick} />
+      </div>
+      <nav className="hidden md:block">
         <ul className="navbar">
           <li>
             <a href="#about">About</a>
@@ -18,7 +32,7 @@ function Header() {
           <li>
             <a href="#pricing">Pricing</a>
           </li>
-          <button className="download-btn">DOWNLOAD NOW</button>
+          <button className="download-btn text-nowrap">DOWNLOAD NOW</button>
         </ul>
       </nav>
     </header>
---
+++ src/components/mobileMenu/mobileMenu.jsx
@@ -0,0 +1,46 @@
+import { IoCloseSharp } from "react-icons/io5";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
+
+function MobileMenu(props) {
+  // key Code 27 is esc, works only if menu is visable.
+  const handleEscKey = (event) => {
+    if (event.keyCode === 27 && props.isMenuOpen) props.closeMenu();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleEscKey);
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  });
+
+  const handleClickOutside = (event) => {
+    if (event.target === event.currentTarget) {
+      props.closeMenu();
+    }
+  };
+  return (
+    <motion.div
+      initial={{ x: "-100%" }}
+      animate={{ x: props.isMenuOpen ? "0%" : "-100%" }}
+      transition={{ ease: "easeOut", duration: 0.5 }}
+      className="fixed top-0 left-0 h-screen w-screen bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg backdrop-saturate-150 text-black z-50 flex justify-center items-center"
+      onClick={handleClickOutside}
+    >
+      <div className="relative text-white flex justify-center min-w-96 p-5 rounded-lg	">
+        <ul className="flex flex-col text-2xl text-center gap-4 py-10">
+          <li>hey</li>
+        </ul>
+        <IoCloseSharp
+          fill="red"
+          size={40}
+          className="absolute top-1 right-1 cursor-pointer"
+          onClick={props.closeMenu}
+        />
+      </div>
+    </motion.div>
+  );
+}
+
+export default MobileMenu;
