62f3128447ccc1a071b0367702743c4275f64ea7
pawel <wydrapawel@icloud.com>
2024-02-01 11:57:38 +0100

    feat: add padding and text color to step card

--- src/components/main/steps/stepsCard.jsx
+++ src/components/main/steps/stepsCard.jsx
@@ -2,11 +2,11 @@ function StepsCard(props) {
     const { number, title, description, linkText, linkHref } = props;
   
     return (
-      <div className="steps-item gap-4 rounded-2xl max-lg:items-center">
+      <div className="steps-item gap-4 rounded-2xl max-lg:items-center *:flex-1">
         <div className="step-number">{number}</div>
         <h2>{title}</h2>
-        <p>{description}</p>
-        <a href={linkHref}>{linkText}</a>
+        <p className="text-gray-400">{description}</p>
+        <a className="font-bold" href={linkHref}>{linkText}</a>
       </div>
     );
   }
