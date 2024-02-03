73ea0628ed384a9ee481f7e454cccac94f24b5a1
pawel <wydrapawel@icloud.com>
2024-02-03 08:36:17 +0100

    refactor: change padding

--- src/components/main/MoreFeatures/MoreFeatures.jsx
+++ src/components/main/MoreFeatures/MoreFeatures.jsx
@@ -4,20 +4,56 @@ import SectionContainer from "../../utils/SectionContainer.jsx";
 import { FaRegLightbulb, FaUserTie } from "react-icons/fa6";
 import { FaBolt, FaBalanceScale } from "react-icons/fa";
 
+const iconContainers = [
+  {
+    icon: (
+      <FaBolt className="icon icon-strong size-14" aria-label="Powerful Icon" />
+    ),
+    title: "Powerful",
+    description: "Send large files without any limitations",
+  },
+  {
+    icon: (
+      <FaRegLightbulb
+        className="icon icon-blob size-14"
+        aria-label="Smart Icon"
+      />
+    ),
+    title: "Smart",
+    description:
+      "Get real-time notifications when someone shares a file with you",
+  },
+  {
+    icon: (
+      <FaBalanceScale
+        className="icon icon-scale size-14"
+        aria-label="Easy Scale Icon"
+      />
+    ),
+    title: "Easy Scale",
+    description: "Access app reviews and ratings from your trusted network",
+  },
+  {
+    icon: (
+      <FaUserTie
+        className="icon icon-professional size-14"
+        aria-label="Professional Icon"
+      />
+    ),
+    title: "Professional",
+    description:
+      "Save time searching for apps with personalized recommendations",
+  },
+];
+
 const MoreFeatures = () => {
   return (
     <SectionContainer>
       <div className="flex flex-col gap-8 ">
-      <span>MORE FEATURES</span>
-      <h1>Explore Our  Business Features</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-3 xl:grid-cols-3 xl:grid-rows-2 gap-10">
-        <div className="flex flex-col gap-4 px-4 py-8">
-          <FaBolt className="icon icon-strong" aria-label="Powerful Icon" />
-          <h2>Powerful</h2>
-          <p>Send large files without any limitations</p>
-        </div>
-
-          <div className="img-holder row-span-2 max-xl:row-start-1 max-xl:col-start-1 max-xl:col-span-full">
+        <span>MORE FEATURES</span>
+        <h1>Explore Our Business Features</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-3 xl:grid-cols-3  xl:grid-rows-2 gap-10">
+          <div className="img-holder xl:col-start-2 row-span-2 max-xl:col-span-full">
             <img
               src={process.env.PUBLIC_URL + "/images/business-img.png"}
               alt="Business"
@@ -34,29 +70,19 @@ const MoreFeatures = () => {
               className="business-user"
             />
           </div>
-
-        <div className="flex flex-col gap-4 px-4 py-8">
-          <FaRegLightbulb className="icon icon-blob" aria-label="Smart Icon" />
-          <h2>Smart</h2>
-          <p>Get real-time notifications when someone shares a file with you</p>
+          {iconContainers.map((container, index) => (
+            <div
+              key={index}
+              className={`flex flex-col gap-4 py-4 ${
+                index === 0 ? "xl:order-first" : ""
+              }`}
+            >
+              {container.icon}
+              <h2>{container.title}</h2>
+              <p>{container.description}</p>
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col gap-4 px-4 py-8">
-          <FaBalanceScale
-            className="icon icon-scale"
-            aria-label="Easy Scale Icon"
-          />
-          <h2>Easy Scale</h2>
-          <p>Access app reviews and ratings from your trusted network</p>
-        </div>
-        <div className="flex flex-col gap-4 px-4 py-8">
-          <FaUserTie
-            className="icon icon-professional"
-            aria-label="Professional Icon"
-          />
-          <h2>Professional</h2>
-          <p>Save time searching for apps with personalized recommendations</p>
-        </div>
-      </div>
       </div>
     </SectionContainer>
   );
