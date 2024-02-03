7f8e5a43384225d73f47d673ce454652dec43586
pawel <wydrapawel@icloud.com>
2024-02-03 08:48:49 +0100

    refactor: change text hight

--- src/components/main/MoreFeatures/MoreFeatures.jsx
+++ src/components/main/MoreFeatures/MoreFeatures.jsx
@@ -50,7 +50,7 @@ const MoreFeatures = () => {
   return (
     <SectionContainer>
       <div className="flex flex-col gap-8 ">
-        <span>MORE FEATURES</span>
+        <span className="self-center lg:self-start">MORE FEATURES</span>
         <h1>Explore Our Business Features</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-3 xl:grid-cols-3  xl:grid-rows-2 gap-10">
           <div className="img-holder xl:col-start-2 row-span-2 max-xl:col-span-full">
--- src/components/main/faq/question.jsx
+++ src/components/main/faq/question.jsx
@@ -5,9 +5,9 @@ const Question = ({ id, question, answer }) => {
     return (
       <div className="flex gap-4" key={id}>
         <BsQuestion className="self-center size-16 max-md:hidden" />
-        <div className="flex-1 *:max-md:text-center">
-          <h3 className="text-xl lg:text-2xl font-bold mb-2">{question}</h3>
-          <p className="max-md:text-md italic">{answer}</p>
+        <div className="flex-1 *:max-md:">
+          <h3 className="text-xl lg:text-2xl font-bold mb-4 text-center">{question}</h3>
+          <p className="sm:text-md xl:text-lg italic text-pretty">{answer}</p>
         </div>
       </div>
     );
